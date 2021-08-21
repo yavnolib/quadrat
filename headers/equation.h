2277856779fe3822ff588c75a5afd71783109cd4
Yaroslav <51954484+yavnolib@users.noreply.github.com>
2021-08-22 01:40:51 +0300

    Update equation.h

--- headers/equation.h
+++ headers/equation.h
@@ -1,20 +1,36 @@
 #pragma once
-#define _CRT_SECURE_NO_WARNINGS
-// Описания функций
-int ABS(int n);                                   // Модуль числа
-int count_x(char* str, size_t length);            // Количество "х" в строке
-size_t last_x_entry(char str[], size_t length);  // Индекс последнего вхождения икса
-size_t first_x_entry(char str[], size_t length); // Индекс первого вхождения икса
-int ten_pow(size_t n);                           // 10^n
-int get_opposite(int n);                         // Смена знака у числа
-size_t pow_ch(char str[], size_t length);        // Индекс значка степени (^)
-int get_ratio(char str[], size_t length, size_t left_index, size_t right_index); // Получить числовое значение коэффициентов a, b, c
-int gcd(int a, int b);
-int reduce_fraction(int a, int b);
-void new_reduce(int* a, int* b);
-char* remove_white_spaces(char* str);
-int is_equal(char* str);
-int RemoveEq(char* str);
-void str_analys(char* str);
-int is_sqr(int n);
-int int_sqrt(int n);
+#include <stdio.h>      
+
+#define WITHOUT_A 55555   
+#define WITHOUT_A_C 66666 
+#define WITHOUT_B 77777 
+#define WITHOUT_B_C 88888 
+#define WITHOUT_C 99999 
+#define FULL 12121 
+
+#define COMPLEX 44444
+#define REAL 33333
+
+#define TWO_ROOTS 12345
+#define ONE_ROOT 11111
+#define TWO_COMP 21313
+
+struct Equation {
+	float a;
+	float b;
+	float c;
+	float x1;
+	float x2;
+	int type;
+	int is_complex;
+	int root;
+};
+void equation_initialize(struct Equation* eq);
+void equation_input(struct Equation* eq);
+void get_eq_type(struct Equation* eq);
+void print_eq_form(struct Equation* eq);
+void solve_eq(struct Equation* eq);
+void print_complex_solution(struct Equation* eq);
+void print_roots(struct Equation* eq);
+void enter_equation(struct Equation* eq);
+void solve_and_print(struct Equation* eq);
