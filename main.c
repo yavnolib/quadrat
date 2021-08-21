14e6cec38434e17f0a6eab45d16b5a5b4c19c57d
Yaroslav <51954484+yavnolib@users.noreply.github.com>
2021-08-22 01:39:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,15 +2,22 @@
 #include <stdlib.h>
 #include <locale.h>     // setlocale ( LC_ALL, "");
 #include <ctype.h>      // isdigit (c)
-#include <string.h>     // strlen (a)
 #include "equation.h"
+//#include <string.h>     // strlen (a)
+
 
 int main(int argc, char* argv[])
 {
     setlocale(LC_ALL, "");
-    char str[100];
-    printf("\nEnter a eq : ");
-    fgets(str, 100, stdin);
-    str_analys(str);
+
+    //char str[100];
+    //printf("\nEnter a eq : ");
+    //fgets(str, 100, stdin);
+    //str_analys(str);
+    //return 0;
+    struct Equation quadro;
+    enter_equation(&quadro);
+    solve_and_print(&quadro);
     return 0;
 }
+
