1fe7b1eb21ec667734ca4b97006d95a7a0caf804
Yaroslav <51954484+yavnolib@users.noreply.github.com>
2021-08-22 16:03:07 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,17 +3,23 @@
 #include <locale.h>     // setlocale ( LC_ALL, "");
 #include <ctype.h>      // isdigit (c)
 #include "equation.h"
+#include "check_test.h"
 //#include <string.h>     // strlen (a)
 
 
 int main(int argc, char* argv[])
-{
+{   
     setlocale(LC_ALL, "");
-
+    
+    struct Equation test_inp;
+    struct Equation test_out;
+    
+    start_test(&test_inp, &test_out);
     //char str[100];
     //printf("\nEnter a eq : ");
     //fgets(str, 100, stdin);
     //str_analys(str);
+
     //return 0;
     struct Equation quadro;
     equation_initialize(&quadro);
