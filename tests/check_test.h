74d71c21e01541522aa4336209749c88b3c81758
Yaroslav <51954484+yavnolib@users.noreply.github.com>
2021-08-23 18:36:12 +0300

    Update check_test.h

--- tests/check_test.h
+++ tests/check_test.h
@@ -1,11 +1,14 @@
 #pragma once
+
 #include <stdio.h>
 #include "equation.h"
+
+#define TEST_COUNT 10
+
+#define TEST_ARGS 8
 
 void init_test(struct Equation* inp, struct Equation* out, float a, float b, float c, int type, int is_complex, int root, float x1, float x2);
 
 int check_ans(struct Equation* inp, struct Equation* out);
-
-int is_equalf(float a, float b);
 
 void start_test(struct Equation* input, struct Equation* output);
