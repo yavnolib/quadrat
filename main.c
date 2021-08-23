259fd7289827f8562ef3fa726fa34cfb88e5af0b
Yaroslav <51954484+yavnolib@users.noreply.github.com>
2021-08-23 18:35:17 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
-#include <stdio.h>      // ввод-вывод
+/*! \mainpage QUADRAT
+ *
+ * \section intro_sec Introduction
+ * This program allows you to solve quadratic and linear equations, displays the answer in fractional numbers, rounded to two decimal places.
+ * - The program can solve equations with complex roots.
+ * - You can enter multiple equations if you like. The program will ask you if you want to enter a new equation or not (y/n?)
+ */
+
+#include <stdio.h>      
 #include <stdlib.h>
-#include <locale.h>     // setlocale ( LC_ALL, "");
-#include <ctype.h>      // isdigit (c)
+#include <locale.h>     
+#include <ctype.h>      
 #include "equation.h"
 #include "check_test.h"
-//#include <string.h>     // strlen (a)
 
 
 int main(int argc, char* argv[])
@@ -13,21 +20,18 @@ int main(int argc, char* argv[])
     
     struct Equation test_inp;
     struct Equation test_out;
+
+    struct Equation quadro;
     
     start_test(&test_inp, &test_out);
-    //char str[100];
-    //printf("\nEnter a eq : ");
-    //fgets(str, 100, stdin);
-    //str_analys(str);
-
-    //return 0;
-    struct Equation quadro;
+    
     do {
         equation_initialize(&quadro);
         enter_equation(&quadro);
         solve_eq(&quadro);
         print_roots(&quadro);
     } while (want_again());
+
     return 0;
 }
 
