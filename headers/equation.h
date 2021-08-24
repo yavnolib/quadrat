dbc37770d27f137a59c80775c6dad5c9fbe00f0d
Yaroslav <51954484+yavnolib@users.noreply.github.com>
2021-08-24 19:11:16 +0300

    Update equation.h

--- headers/equation.h
+++ headers/equation.h
@@ -2,64 +2,58 @@
 /*! \file equation.h
  * Equation's properties. */
 
-
 #include <stdio.h>      
 
- //! constant for number comparisons
-#define PRECISION 1e-6
-
-
- //! constant for equation without a
-#define WITHOUT_A 55555
-
- //! constant for equation without a and c
-#define WITHOUT_A_C 66666
-
- //! constant for equation without b
-#define WITHOUT_B 77777 
-
- //! constant for equation without b and c
-#define WITHOUT_B_C 88888
-
- //! constant for equation without c
-#define WITHOUT_C 99999
-
- //! constant for full equation
-#define FULL 12121
-
- //! constant for complex solutions
-#define COMPLEX 44444
-
- //! constant for real solutions
-#define REAL 33333
-
- //! constant for equations with two solutions
-#define TWO_ROOTS 12345
-
- //! constant for equations with one solution
-#define ONE_ROOT 11111
-
- //! constant for equations with two complex solutions
-#define TWO_COMP 21313
-
-//! constant for empty root
-#define EMPTY_R 85585
-
-//! constant for complex root
-#define COMPLEX_R 56794
-
-//! \struct Equation "equation.h"
-/*! Structure of the equation, which include equation'c coefficients, roots and equation's type. */
+ //! \struct Equation "equation.h"
+ /*! Structure of the equation, which include equation'c coefficients, roots and equation's type. */
 struct Equation {
-	float a; /*!< Coefficient with x^2. */
-	float b; /*!< Coefficient with x^1. */
-	float c; /*!< Coefficient with x^0. */
-	float x1; /*!< First possible root. */
-	float x2; /*!< Second possible root. */
-	int type; /*!< Type of equation. */
-	int is_complex; /*!< Root type. */
-	int root; /*!< Number of roots. */
+    float a; /*!< Coefficient with x^2. */
+    float b; /*!< Coefficient with x^1. */
+    float c; /*!< Coefficient with x^0. */
+    float x1; /*!< First possible root. */
+    float x2; /*!< Second possible root. */
+    int type; /*!< Type of equation. */
+    int is_complex; /*!< Root type. */
+    int root; /*!< Number of roots. */
 };
+
+//! \enum x_type "equation.h"
+/*! Constants for type of root */
+enum x_type {
+    EMPTY_X = 22222, /*!< Constant for empty root */
+    COMPLEX_X        /*!< Constant for complex root */
+};
+
+//! \enum x_type "equation.h"
+/*! Constants for number of roots */
+enum root_type {
+    ONE_ROOT = 11111, /*!< Constant for equation with one root */
+    NO_ROOTS_R,       /*!< Constant for equation without roots */
+    TWO_ROOTS,        /*!< Constant for equation with two roots */
+    TWO_COMP          /*!< Constant for equation with two complex roots */
+};
+
+//! \enum x_type "equation.h"
+/*! Constants for type of equation */
+enum equation_type {
+    WITHOUT_ALL = 88888, /*!< Constant for equation without a,b,c coefficients */
+    NO_ROOTS_TYPE,       /*!< Constant for equation without roots */
+    WITHOUT_A,           /*!< Constant for equation without a coefficient */
+    WITHOUT_A_C,         /*!< Constant for equation without a, c coefficients */        
+    WITHOUT_B,           /*!< Constant for equation without b coefficient */
+    WITHOUT_B_C,         /*!< Constant for equation without b, c coefficients */
+    WITHOUT_C,           /*!< Constant for equation without c coefficient */
+    FULL                 /*!< Constant for equation with all coefficients */
+};
+
+//! \enum x_type "equation.h"
+/*! Constants for type of equation's roots */
+enum iscomplex_type {
+    COMPLEX = 33333,     /*!< Constant for equation with complex roots */
+    REAL,                /*!< Constant for equation with not complex roots */
+    NO_ROOTS_IS          /*!< Constant for equation without routs */
+};
+
 
 //! Initializes equation, leaving all its members in valid state
 //! before input is placed.
@@ -96,3 +90,11 @@ void enter_equation(struct Equation* eq);
 
 //! A function that asks if the user wants to continue
 int want_again();
+
+//! A function that tests the equality of two float numbers
+/*! \param equation - two float numbers. */
+int is_equalf(float a, float b);
+
+//! A function that tests the equality of two integer numbers
+/*! \param equation - two integer numbers. */
+int is_equali(int a, int b);
