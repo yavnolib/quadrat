f852931bf4a789a2cce07bb551f00037ccf3c535
Yaroslav <51954484+yavnolib@users.noreply.github.com>
2021-08-25 16:38:02 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,22 +5,16 @@
  * - The program can solve equations with complex roots.
  * - You can enter multiple equations if you like. The program will ask you if you want to enter a new equation or not (y/n?)
  */
-
 #include <stdio.h>      
-#include <stdlib.h>
-#include <locale.h>     
+#include <stdlib.h>   
 #include <ctype.h>      
 #include "equation.h"
 #include "check_test.h"
 
-
 int main(int argc, char* argv[])
 {   
-    setlocale(LC_ALL, "");
-    
     struct Equation test_inp;
     struct Equation test_out;
-
     struct Equation quadro;
     
     start_test(&test_inp, &test_out);
